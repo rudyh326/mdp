7366db509f110c37a9601b93587d527e01f425b7
rudyh326 <hneinerudy9@gmail.com>
2022-03-08 22:33:01 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "Hello World!"
+	return jsonify("Hello World!")
+
+@app.route('/bring',methods=['POST'])
+def bring_req():
+	req_data = request.get_json()
+	return jsonify(req_data)
 
 if __name__ == '__main__':
     app.run(debug = True)
