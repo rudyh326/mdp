cdadac14ac5cd295b0ec01ca07f951cfbaf6207a
rudyh326 <hneinerudy9@gmail.com>
2022-03-10 17:22:25 +0200

    first commit

--- app.py
+++ app.py
@@ -6,10 +6,20 @@ app = Flask(__name__)
 def home():
 	return jsonify("Hello World!")
 
-@app.route('/bring',methods=['POST'])
-def bring_req():
+req_data=0
+@app.route('/data',methods=['POST'])
+def data_req():
+	global req_data
 	req_data = request.get_json()
 	return jsonify(req_data)
+
+print(req_data)
+# @app.route('/data/<string:name>')
+# def get_data(name):
+# 	return jsonify(name)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
